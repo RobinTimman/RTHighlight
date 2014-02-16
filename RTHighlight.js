c84fe50c017623f8efdce470073f2b43bcd15a1a
Robin Timman <info@robin>
2014-02-16 15:19:29 +0100

    Code update

--- RTHighlight.js
+++ RTHighlight.js
@@ -2,7 +2,7 @@ var s, RTHighlight = {
 	
 	settings: {
 		tab: '<tb></tb>',/*'&nbsp;&nbsp;',*/
-		nl: ["input", "br", "h1", "img", "a"],	// ends on the same line
+		nl: ["input", "br", "h1", "img", "a", "label"],	// ends on the same line
 		endNodes: ["input", "br", "img"],	// ends with / in same line
 	},
 	content: '',
@@ -85,6 +85,10 @@ var s, RTHighlight = {
 		if(el.value) 			attributes 			+= ' '+this.genSpan('RTH-a', 'value=')+this.genSpan('RTH-at', '"'+el.type+'"');
 		// if name
 		if(el.name) 			attributes 			+= ' '+this.genSpan('RTH-a', 'name=')+this.genSpan('RTH-at', '"'+el.name+'"');
+		// if alt
+		if(el.alt) 				attributes 			+= ' '+this.genSpan('RTH-a', 'alt=')+this.genSpan('RTH-at', '"'+el.alt+'"');
+		// if for
+		if(el.htmlFor) 			attributes 			+= ' '+this.genSpan('RTH-a', 'for=')+this.genSpan('RTH-at', '"'+el.htmlFor+'"');
 		// if src (source)
 		if(el.src) 				attributes 			+= ' '+this.genSpan('RTH-a', 'src=')+this.genSpan('RTH-at', '"'+el.src+'"');
 		if(el.getAttribute('RTH-src', 2)){
@@ -99,6 +103,16 @@ var s, RTHighlight = {
 		if(el.getAttribute('RTH-action', 2)){
 								attributes 			+= ' '+this.genSpan('RTH-a', 'action=')+this.genSpan('RTH-at', '"'+el.getAttribute('RTH-action', 2)+'"');
 		}
+		// if title
+		if(el.title) 			attributes 			+= ' '+this.genSpan('RTH-a', 'titel=')+this.genSpan('RTH-at', '"'+el.title+'"');
+		// if title
+		if(el.download) 		attributes 			+= ' '+this.genSpan('RTH-a', 'download=')+this.genSpan('RTH-at', '"'+el.download+'"');
+		// if title
+		if(el.hidden) 			attributes 			+= ' '+this.genSpan('RTH-a', 'hidden=')+this.genSpan('RTH-at', '"'+el.hidden+'"');
+		// if title
+		if(el.target) 			attributes 			+= ' '+this.genSpan('RTH-a', 'target=')+this.genSpan('RTH-at', '"'+el.target+'"');
+		// if title
+		if(el.rel) 			attributes 			+= ' '+this.genSpan('RTH-a', 'rel=')+this.genSpan('RTH-at', '"'+el.rel+'"');
 		// for each style attr
 		if(el.style[0])			attributes			+= ' '+this.getStyles(el)+'';
 
